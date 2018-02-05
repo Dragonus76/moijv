feca5204c473e7021041b9b05836b23412d5b140
Mustaphasadki <msadki11@gmail.com>
2018-02-05 17:48:17 +0100

    Add user fixture

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixture extends Fixture {
+
+    public function load(ObjectManager $manager) {
+        for ($i = 1; $i < 20; $i++) {
+            $user = new User();
+            $user->setUsername('user' . $i);
+            $user->setEmail('user' . $i . '@mail.com');
+            $user->setFirstname('User' . $i);
+            $user->setLastname('Fake');
+
+            $user->setPassword(password_hash('user' . $i, PASSWORD_BCRYPT));
+
+            $user->setBirthdate(\DateTime::createFromFormat('Y/m/d h:i:s', (2000 - $i) . '/01/01 00:00:00'));
+            //on demande au manager d'enregistrer l'utilisateur en bases de données
+            $manager->persist($user);
+        }
+        $manager->flush(); // les INSERT INTO ne sont effectués qu'a ce moment la
+    }
+
+}
