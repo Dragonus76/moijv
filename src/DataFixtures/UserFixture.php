d24124168e8f6f0204e778ca53016758abdc9e9b
Mustaphasadki <msadki11@gmail.com>
2018-02-07 11:50:39 +0100

    ADD products in database and display them in admin

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -9,7 +9,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 class UserFixture extends Fixture {
 
     public function load(ObjectManager $manager) {
-        for ($i = 1; $i < 20; $i++) {
+        for ($i = 1; $i <= 20; $i++) {
             $user = new User();
             $user->setUsername('user' . $i);
             $user->setEmail('user' . $i . '@mail.com');
@@ -19,8 +19,13 @@ class UserFixture extends Fixture {
             $user->setPassword(password_hash('user' . $i, PASSWORD_BCRYPT));
 
             $user->setBirthdate(\DateTime::createFromFormat('Y/m/d h:i:s', (2000 - $i) . '/01/01 00:00:00'));
-            //on demande au manager d'enregistrer l'utilisateur en bases de données
+            //on demande au manager  d'enregistrer l'utilisateur en bases de données
+            //notre user référencé dans les autres fixtures sous la clé
+            //user0 puis userl puis user
+            
+            $this->addReference('user'.$i, $user);
             $manager->persist($user);
+            
         }
         $manager->flush(); // les INSERT INTO ne sont effectués qu'a ce moment la
     }
