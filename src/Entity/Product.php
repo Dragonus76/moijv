8186a50ff5cf60ecab4038a0ee7da4e6af75894c
Mustaphasadki <msadki11@gmail.com>
2018-02-09 18:00:58 +0100

    add template

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,129 +7,140 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Index;
 use Doctrine\ORM\Mapping\Table;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Validator\Constraints as CustomAssert;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
- * @Table(indexes={@Index(name="state_index", columns={"state"})})
- */
+* @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+* @Table(indexes={@Index(name="state_index", columns={"state"})})
+*/
 class Product {
 
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+   /**
+    * @ORM\Id
+    * @ORM\GeneratedValue
+    * @ORM\Column(type="integer")
+    */
+   private $id;
 
-    /**
-     * @ORM\Column(type="string", length=100)
-     * @var string
-     */
-    private $name;
+   /**
+    * @ORM\Column(type="string", length=100)
+    * @Assert\Length(min=2, max=100)
+    * @var string
+    */
+   private $name;
 
-    /**
-     * @ORM\Column(type="text")
-     * @var string
-     */
-    private $description;
+   /**
+    * @ORM\Column(type="text")
+    * @Assert\Length(min=20, max=2000)
+    * @CustomAssert\BadWord(badwords={"crotte","zut"})
+    * @var string
+    */
+   private $description;
 
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $image;
+   /**
+    * @ORM\Column(type="string")
+    * @Assert\Image(minWidth=300, minHeight=300, maxSize="1024k")
+    */
+   private $image;
 
-    /**
-     * @ORM\Column(type="string", length=20)
-     * @var string 
-     */
-    private $state;
+   /**
+    * @ORM\Column(type="string", length=20)
+    * @Assert\Choice({"very good", "good", "average", "bad", "very bad"})
+    * @var string
+    */
+   private $state;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="User", inversedBy="products")
-     * @var User
-     */
-    private $user;
+   /**
+    * @ORM\ManyToOne(targetEntity="User", inversedBy="products")
+    * @var User
+    */
+   private $user;
 
-    /**
-     * @ORM\ManyToMany(targetEntity="Tag", mappedBy="products")
-     * @var Collection
-     */
-    private $tags;
+   /**
+    * @ORM\ManyToMany(targetEntity="Tag", mappedBy="products")
+    * @var Collection
+    */
+   private $tags;
 
-    public function __construct() {
-        $this->tags = new ArrayCollection;
-    }
+   public function __construct() {
+       $this->tags = new ArrayCollection();
+   }
 
-    public function addTag($tag) {
-        if ($this->getTags()->contains($tag)) {
-            return;
-        }
-        
-        $this->getTags()->add($tag);
-        $tag->getProducts()->add($this);
-    }
+   public function addTag($tag) {
+       if ($this->getTags()->contains($tag)) {
+           return;
+       }
 
-    public function getTags(): Collection {
-        return $this->tags;
-    }
+       $this->getTags()->add($tag);
+       $tag->getProducts()->add($this);
+   }
 
-    public function setTags(Collection $tags) {
-        $this->tags = $tags;
-        return $this;
-    }
+   public function getTags(): Collection {
+       return $this->tags;
+   }
 
-    public function getUser(): User {
-        return $this->user;
-    }
+   public function setTags(Collection $tags) {
+       $this->tags = $tags;
+       return $this;
+   }
 
-    public function setUser(User $user) {
-        $this->user = $user;
-        return $this;
-    }
+   public function getId() {
+       return $this->id;
+   }
 
-    public function getId() {
-        return $this->id;
-    }
+   public function getName() {
+       return $this->name;
+   }
 
-    public function getName() {
-        return $this->name;
-    }
+   public function getDescription() {
+       return $this->description;
+   }
 
-    public function getDescription() {
-        return $this->description;
-    }
+   public function getImage() {
+       return $this->image;
+   }
 
-    public function getImage() {
-        return $this->image;
-    }
+   public function getState() {
+       return $this->state;
+   }
 
-    public function getState() {
-        return $this->state;
-    }
+   public function getUser(): User {
+       return $this->user;
+   }
 
-    public function setId($id) {
-        $this->id = $id;
-        return $this;
-    }
+   public function setId($id) {
+       $this->id = $id;
+       return $this;
+   }
 
-    public function setName($name) {
-        $this->name = $name;
-        return $this;
-    }
+   public function setName($name) {
+       $this->name = $name;
+       return $this;
+   }
 
-    public function setDescription($description) {
-        $this->description = $description;
-        return $this;
-    }
+   public function setDescription($description) {
+       $this->description = $description;
+       return $this;
+   }
 
-    public function setImage($image) {
-        $this->image = $image;
-        return $this;
-    }
+   public function setImage($image) {
+       $this->image = $image;
+       return $this;
+   }
 
-    public function setState($state) {
-        $this->state = $state;
-        return $this;
-    }
+   public function setState($state) {
+       $this->state = $state;
+       return $this;
+   }
+
+   public function setUser(User $user) {
+       $this->user = $user;
+       return $this;
+   }
 
 }
+
+
+
+
